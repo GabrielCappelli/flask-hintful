2a5f852a324ffe726b017bc9140c1fa1b5d58b27
Gabriel Cappelli <6148081+GabrielCappelli@users.noreply.github.com>
2019-09-10 19:08:52 -0300

    Refacotors OpenApi tests

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -3,35 +3,27 @@ from flask_hintful import FlaskHintful
 from flask_hintful.openapi import OpenApiProvider
 
 
-def test_openapi_json(api, marshmallow_type, dataclass_type):
-    '''Should return openapi json
+def test_openapi_json(api):
+    '''Should return openapi json from custom url
     '''
-    @api.route('/empty_route')
-    def empty_route():
-        pass
+    app = Flask(__name__)
+    app.config['FLASK_HINTFUL_OPENAPI_JSON_URL'] = '/custom_openapi.json'
 
-    @api.route('/<path_arg>')
-    def simple_route(query_arg: str, path_arg) -> str:
-        pass
-
-    @api.route('/marshmallow_test', methods=['POST'])
-    def marshmallow_route(model: marshmallow_type) -> marshmallow_type:
-        pass
-
-    @api.route('/dataclass_test', methods=['POST'])
-    def dataclass_route(model: dataclass_type) -> dataclass_type:
-        pass
-
+    api = FlaskHintful(app)
     with api.flask_app.test_client() as client:
-        response = client.get('/openapi.json')
+        response = client.get('/custom_openapi.json')
         assert response.get_json() is not None
 
 
 def test_openapi_ui(api):
-    '''Should return openapi ui
+    '''Should return openapi ui from custom url
     '''
+    app = Flask(__name__)
+    app.config['FLASK_HINTFUL_OPENAPI_UI_URL'] = '/custom_swagger'
+
+    api = FlaskHintful(app)
     with api.flask_app.test_client() as client:
-        response = client.get('/swagger')
+        response = client.get('/custom_swagger')
         assert response.get_data(as_text=True) is not None
 
 
@@ -51,3 +43,56 @@ def test_openapi_blueprint():
 
     assert openapi.openapi_paths[0].path == '/bp_route/{id}'
     assert openapi.openapi_paths[0].method == 'get'
+
+
+def test_openapi_args():
+    '''Should successfully register query and path args
+    '''
+    openapi = OpenApiProvider()
+    app = Flask(__name__)
+    api = FlaskHintful(app, openapi_provider=openapi)
+
+    @api.route('/route/<id>')
+    def api_route(id: str, foo: str = 'bar') -> str:
+        pass
+
+    assert openapi.openapi_paths[0].path == '/route/{id}'
+    assert openapi.openapi_paths[0].method == 'get'
+    assert openapi.openapi_paths[0].responses[0].data_type == str
+
+    assert openapi.openapi_paths[0].params[0].name == 'id'
+    assert openapi.openapi_paths[0].params[0].data_type == str
+    assert openapi.openapi_paths[0].params[0].location == 'path'
+    assert openapi.openapi_paths[0].params[0].required
+
+    assert openapi.openapi_paths[0].params[1].name == 'foo'
+    assert openapi.openapi_paths[0].params[1].data_type == str
+    assert openapi.openapi_paths[0].params[1].location == 'query'
+    assert not openapi.openapi_paths[0].params[1].required
+    assert openapi.openapi_paths[0].params[1].default == 'bar'
+
+
+def test_openapi_request_body(marshmallow_type, dataclass_type):
+    '''Should successfully register objects as request body
+    '''
+    openapi = OpenApiProvider()
+    app = Flask(__name__)
+    api = FlaskHintful(app, openapi_provider=openapi)
+
+    @api.route('/marshmallow_test', methods=['POST'])
+    def marshmallow_route(_: marshmallow_type) -> marshmallow_type:
+        pass
+
+    @api.route('/dataclass_test', methods=['POST'])
+    def dataclass_route(_: dataclass_type) -> dataclass_type:
+        pass
+
+    assert openapi.openapi_paths[0].path == '/marshmallow_test'
+    assert openapi.openapi_paths[0].method == 'post'
+    assert openapi.openapi_paths[0].responses[0].data_type == marshmallow_type.__marshmallow__
+    assert openapi.openapi_paths[0].request_body == marshmallow_type.__marshmallow__
+
+    assert openapi.openapi_paths[1].path == '/dataclass_test'
+    assert openapi.openapi_paths[1].method == 'post'
+    assert openapi.openapi_paths[1].responses[0].data_type == dataclass_type
+    assert openapi.openapi_paths[1].request_body == dataclass_type
